69b4fe98e835ccde186f1eece0c42e511e3213bb
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2024-10-04 20:18:06 +0200

    fixed listing interfaces

--- lib/load-bindings.ts
+++ lib/load-bindings.ts
@@ -6,21 +6,9 @@ import {join} from 'path'
 const binding = nodeGypBuild(join(__dirname, '../')) as any;
 
 // Promisify the native methods or provide a fallback that throws an error
-export const asyncClose = binding.close ? promisify(binding.close) : async () => {
-    throw new Error('"binding.close" Method not implemented');
-};
-export const asyncWrite = binding.write ? promisify(binding.write) : async () => {
-    throw new Error('"binding.write" Method not implemented');
-};
-export const asyncRead = binding.read ? promisify(binding.read) : async () => {
-    throw new Error('"binding.read" Method not implemented');
-};
-export const asyncListCanDevices = binding.listCanDevices ? promisify(binding.listCanDevices) : async () => {
-    throw new Error('"binding.listCanDevices" Method not implemented');
-};
-export const asyncOpenCanChannel = binding.openCanChannel ? promisify(binding.openCanChannel) : async () => {
-    throw new Error('"binding.openCanChannel" Method not implemented');
-};
-export const asyncSetMessageCallback = binding.setMessageCallback ? promisify(binding.setMessageCallback) : async () => {
-    throw new Error('"binding.setMessageCallback" Method not implemented');
-};
+export const asyncListCanDevices = binding.list  ? promisify(binding.list ) : async () => { throw new Error('"binding.list " Method not implemented')}
+export const asyncOpenCanChannel = binding.open  ? promisify(binding.open ) : async () => { throw new Error('"binding.open " Method not implemented')}
+export const asyncClose = binding.close  ? promisify(binding.close ) : async () => { throw new Error('"binding.close " Method not implemented')}
+export const asyncRead = binding.read  ? promisify(binding.read ) : async () => { throw new Error('"binding.read " Method not implemented')}
+export const asyncSetMessageCallback = binding.setCallback  ? promisify(binding.setCallback ) : async () => { throw new Error('"binding.setCallback " Method not implemented')}
+export const asyncWrite = binding.write  ? promisify(binding.write ) : async () => { throw new Error('"binding.write " Method not implemented')}
